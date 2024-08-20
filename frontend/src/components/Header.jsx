35b72b4be8137543edf075460ffaf3b1f4e6dc43
rickeymaharjan <rickeymhrjn23@gmail.com>
2024-08-20 08:18:52 +0545

    Add responsiveness to header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,71 @@
+// Componenents from ShadCN
 import { Button } from "./ui/button"
 import { Input } from "./ui/input"
 import { Link } from "react-router-dom"
 
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+import { Avatar, AvatarImage, AvatarFallback } from "@radix-ui/react-avatar"
+
+// Icons
 import { IoSearchOutline } from "react-icons/io5"
+import { IoSettingsOutline } from "react-icons/io5"
+import { IoLogInOutline } from "react-icons/io5"
+import { IoPersonOutline } from "react-icons/io5"
+
+// Redux
+import { useSelector } from "react-redux"
+import { useDispatch } from "react-redux"
+import { logout } from "../features/authSlice"
+
+import { useNavigate } from "react-router-dom"
 
 function Header() {
+  const user = useSelector((state) => state.auth.user)
+
+  const isAuthenticated = useSelector((state) => state.auth.isAuthenticated)
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+
+  const handleLogoutClick = () => {
+    dispatch(logout())
+    navigate("/")
+  }
+
+  const handleProfileClick = () => {
+    navigate(`/${user.username}`)
+  }
+
   return (
-    <div className="w-full h-[85px] flex justify-between items-center px-[80px]">
-      <div className="flex gap-7">
-        <p className="font-avenir-medium">Explore recipes</p>
-        <p className="font-avenir-medium">Favourite</p>
-        <p className="font-avenir-medium">Meal plans</p>
+    <div className="w-full h-[75px] drop-shadow-sm flex justify-between items-center common-padding bg-white">
+      <div className="lg:flex gap-7 hidden">
+        <p className="font-avenir-medium font-bold text-gray-500 hover:text-black cursor-pointer">
+          Explore recipes
+        </p>
+        <p className="font-avenir-medium font-bold text-gray-500 hover:text-black cursor-pointer">
+          Favourite
+        </p>
+        <p className="font-avenir-medium font-bold text-gray-500 hover:text-black cursor-pointer">
+          Meal plans
+        </p>
       </div>
 
       <Link
         to="/"
-        className="text-3xl text-black transition ease-in-out font-logo hover:text-[#DE3725] hover:scale-105"
+        className="text-2xl lg:text-3xl text-black transition ease-in-out font-logo hover:text-[#DE3725] hover:scale-105"
       >
         Fresh Feast
       </Link>
 
-      <div className="flex items-center gap-3">
+      <div className="flex items-center gap-1">
         <div className="relative w-[225px] h-[35px]">
           <Input
             className="w-full h-full pl-10 rounded-full "
@@ -30,18 +75,77 @@ function Header() {
             <IoSearchOutline />
           </div>
         </div>
+        <div className="flex items-center gap-2"></div>
 
-        <div className="flex gap-1">
-          <Link to="/login">
-            <Button className="text-black bg-transparent rounded-full hover:bg-transparent">
-              Log in
-            </Button>
-          </Link>
+        {!isAuthenticated ? (
+          <div className="flex gap-1">
+            <Link to="/login">
+              <Button className="text-black bg-transparent rounded-full hover:bg-transparent">
+                Log in
+              </Button>
+            </Link>
 
-          <Link to="/signup">
-            <Button className="rounded-full">Sign up</Button>
-          </Link>
-        </div>
+            <Link to="/signup">
+              <Button className="rounded-full">Sign up</Button>
+            </Link>
+          </div>
+        ) : (
+          <DropdownMenu>
+            <DropdownMenuTrigger className="border w-8 h-8 rounded-full">
+              <Avatar className="w-full h-full">
+                {user.profileImage && (
+                  <AvatarImage
+                    className="rounded-full"
+                    src={user.profileImage}
+                  />
+                )}
+                <AvatarFallback>{user.username[0]}</AvatarFallback>
+              </Avatar>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent>
+              <DropdownMenuLabel>
+                <div className="flex gap-12">
+                  <div className="flex items-center gap-3">
+                    <Avatar className="w-7 h-7">
+                      {user.profileImage && (
+                        <AvatarImage
+                          className="rounded-full"
+                          src={user.profileImage}
+                        />
+                      )}
+                      <AvatarFallback>{user.username[0]}</AvatarFallback>
+                    </Avatar>
+                    <div>
+                      <p className="font-avenir-medium">{user.username}</p>
+                      <p className="text-gray-500">{user.email}</p>
+                    </div>
+                  </div>
+                  <div className="w-1 h-1"></div>
+                </div>
+              </DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="flex gap-2 items-center"
+                onClick={handleProfileClick}
+              >
+                <IoPersonOutline />
+                Profile
+              </DropdownMenuItem>
+              <DropdownMenuItem className="flex gap-2 items-center">
+                <IoSettingsOutline />
+                Settings
+              </DropdownMenuItem>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem
+                className="flex gap-2 items-center"
+                onClick={handleLogoutClick}
+              >
+                <IoLogInOutline />
+                Logout
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        )}
       </div>
     </div>
   )
