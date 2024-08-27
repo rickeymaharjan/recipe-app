83a1485c98a4270de4b76e40aeb900ffa03a1b72
rickeymaharjan <rickeymhrjn23@gmail.com>
2024-08-27 17:53:07 +0545

    Add functionality to remove recipes and change the name of the collection

--- frontend/src/pages/Collection.jsx
+++ frontend/src/pages/Collection.jsx
@@ -3,12 +3,16 @@ import {
   DialogContent,
   DialogDescription,
   DialogHeader,
+  DialogFooter,
   DialogTitle,
   DialogTrigger,
+  DialogClose,
 } from "@/components/ui/dialog"
 import { Label } from "@/components/ui/label"
 import { Avatar, AvatarImage, AvatarFallback } from "@/components/ui/avatar"
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { toast } from "sonner"
 
 import axios from "axios"
 import { useEffect, useState } from "react"
@@ -16,6 +20,8 @@ import { useParams, useNavigate } from "react-router-dom"
 
 import ProfileSkeleton from "@/components/profile/ProfileSkeleton"
 import RecipeList from "@/components/recipePage/RecipeList"
+
+import { IoTrashOutline } from "react-icons/io5"
 
 const Collection = ({ loggedInUsername }) => {
   const { username, collectionId } = useParams()
@@ -25,6 +31,8 @@ const Collection = ({ loggedInUsername }) => {
   const [collectionData, setCollectionData] = useState(null)
   const [loading, setLoading] = useState(true)
   const [error, setError] = useState(null)
+
+  const [collectionName, setCollectionName] = useState("")
 
   const isOwner = username === loggedInUsername
 
@@ -57,6 +65,7 @@ const Collection = ({ loggedInUsername }) => {
             import.meta.env.VITE_BACKEND_URL
           }/api/collections/recipes/${collectionId}`
         )
+        setCollectionName(response.data.name)
         setCollectionData(response.data)
       } catch (error) {
         setError(error.response?.data?.error || "An error occurred")
@@ -68,6 +77,7 @@ const Collection = ({ loggedInUsername }) => {
     fetchCollectionData()
   }, [collectionId])
 
+  // Handle delete collection
   const handleDelete = async () => {
     try {
       await axios.delete(
@@ -76,6 +86,49 @@ const Collection = ({ loggedInUsername }) => {
       navigate(`/${loggedInUsername}/collections`, {
         state: { deleted: true },
       })
+    } catch (error) {
+      setError(error.response?.data?.error || "An error occurred")
+    }
+  }
+
+  // Handle removing a recipe from a collection
+  const handleRemove = async (recipeId) => {
+    try {
+      await axios.delete(
+        `${
+          import.meta.env.VITE_BACKEND_URL
+        }/api/collections/${collectionId}/remove-recipe/${recipeId}`
+      )
+
+      setCollectionData((prevCollections) => ({
+        ...prevCollections,
+        recipes: prevCollections.recipes.filter(
+          (recipe) => recipe._id !== recipeId
+        ),
+      }))
+    } catch (error) {
+      setError(error.response?.data?.error || "An error occurred")
+    }
+  }
+
+  // Handle clicking "done" button
+  const handleDone = async () => {
+    try {
+      if (collectionName !== collectionData.name) {
+        console.log("Collection name has changed")
+
+        await axios.patch(
+          `${import.meta.env.VITE_BACKEND_URL}/api/collections/${collectionId}`,
+          { name: collectionName }
+        )
+
+        setCollectionData((prev) => ({
+          ...prev,
+          name: collectionName,
+        }))
+
+        toast.success("Collection updated")
+      }
     } catch (error) {
       setError(error.response?.data?.error || "An error occurred")
     }
@@ -106,9 +159,66 @@ const Collection = ({ loggedInUsername }) => {
 
           {isOwner && (
             <div className="flex gap-2">
-              <Button variant="outline" className="rounded-full">
-                Edit Collection
-              </Button>
+              <Dialog>
+                <DialogTrigger asChild>
+                  <Button variant="outline" className="rounded-full">
+                    Edit Collection
+                  </Button>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle>Edit Collection</DialogTitle>
+                    <DialogDescription>
+                      Edit your collection name and recipes.
+                    </DialogDescription>
+                  </DialogHeader>
+                  <div className="grid gap-1.5">
+                    <div>
+                      <Label className="">Collection name</Label>
+                      <Input
+                        value={collectionName}
+                        placeholder="Enter your collection name"
+                        onChange={(e) => setCollectionName(e.target.value)}
+                      />
+                    </div>
+                  </div>
+
+                  <div className="grid gap-1.5">
+                    <Label>Recipes</Label>
+                    <div className="flex flex-col gap-2">
+                      {collectionData.recipes.length ? (
+                        collectionData.recipes.map((recipe) => (
+                          <div
+                            key={recipe._id}
+                            className="text-gray-500 hover:text-black justify-between items-center flex border rounded-md p-3 shadow-sm hover:shadow-md transition-all"
+                          >
+                            <Label className="">{recipe.title}</Label>
+                            <IoTrashOutline
+                              className="cursor-pointer"
+                              onClick={() => handleRemove(recipe._id)}
+                            />
+                          </div>
+                        ))
+                      ) : (
+                        <Label className="text-gray-500">
+                          No recipes in this collection yet.
+                        </Label>
+                      )}
+                    </div>
+                  </div>
+                  <DialogFooter>
+                    <DialogClose asChild>
+                      <Button
+                        className="rounded-full"
+                        variant="outline"
+                        onClick={handleDone}
+                      >
+                        Done
+                      </Button>
+                    </DialogClose>
+                  </DialogFooter>
+                </DialogContent>
+              </Dialog>
 
               <Dialog>
                 <DialogTrigger asChild>
